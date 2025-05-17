ef1e0830acb025f43b6cb286d6e5559ae2aa6a6e
Zheng Xu <zhengxu7806@gmail.com>
2025-05-17 15:58:58 -0400

    open thread, get thread content and close

--- src/App.jsx
+++ src/App.jsx
@@ -71,14 +71,47 @@ function App() {
     }
   }
 
+  const openAndCloseThread = async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (tab.id) {
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+            window._threadButtonList = document.querySelectorAll('.c-message__reply_count');
+            if (window._threadButtonList && window._threadButtonList.length > 0) {
+                try {
+                    const button = window._threadButtonList[0];
+                    button.click();
+                    setTimeout(() => {
+                        let threadContentList = document.querySelectorAll('[data-qa="slack_kit_list"].c-virtual_list__scroll_container[role="list"][aria-label^="Thread"] > div');
+                        console.log('threadContentList', threadContentList);
+                        let closeButton = document.querySelector('[aria-label="Close"]');
+                        console.log('closeButton', closeButton);
+                        if (closeButton) {
+                            closeButton.click();
+                        }
+                    }, 1000);
+                } catch (e) {
+                    console.error("Error opening thread:", e);
+                }
+            }
+        }
+      });
+    }
+  }
+
   return (
     <>
       <div className="card">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={scrollUpSlack}>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-2 rounded" onClick={scrollUpSlack}>
           Scroll up
         </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={getPostList}>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-2 rounded" onClick={getPostList}>
           Get Post List
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-2 rounded" onClick={openAndCloseThread}>
+          Open and close thread
         </button>
       </div>
     </>
